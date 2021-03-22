6321adb3d935e8c1f1bc4e6c3d04fb64c8e7bb45
Nicolas Codonho Scorpelini <50plantasquemudaramahistoria@gmail.com>
2021-03-22 17:31:39 -0300

    configurando multer para uploads

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
+
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post('/', (request, response) => {
     return createCategoryController.handle(request, response);
@@ -10,6 +16,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
     return listCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
