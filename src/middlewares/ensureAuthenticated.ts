8b89aab5d7f7588e9131ec0b32cb4ebc432c880d
Nicolas Codonho Scorpelini <50plantasquemudaramahistoria@gmail.com>
2021-03-27 20:15:20 -0300

    Created and implemented avatar system

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import {Request, Response, NextFunction} from "express";
+import {verify} from "jsonwebtoken";
+import { AppErrors } from "../errors/AppErrors";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload{
+    sub: string;
+}
+
+export async function ensureAuthenticated(request:Request, response: Response, next: NextFunction){
+    const authHeader = request.headers.authorization;
+    if(!authHeader){
+        throw new AppErrors("Token missing", 401)
+    }
+
+    const [, token] = authHeader.split(" ");
+    try {
+        const { sub: user_id } = verify(token, "5f75c99215711a703663174ec2ae4979") as IPayload;
+        const usersRepository = new UsersRepository();
+        const user = usersRepository.findById(user_id);
+
+        if(!user){
+            throw new AppErrors("User does not exists", 401);
+        }
+
+        request.user = {
+            id: user_id
+        };
+        
+        next();
+    } catch {
+        throw new AppErrors("Invalid token", 401);
+    }
+}
